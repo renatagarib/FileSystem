81c5a57300ed66cec1664dc74d6c8285acb73f3f
Renata Garib <renatagarib@usp.br>
2022-07-01 21:57:06 -0300

    FileManager class beginning

---
+++ src/FileManager.java
@@ -0,0 +1,50 @@
+import exceptions.InvalidEntryException;
+import exceptions.InvalidSNodeException;
+import exceptions.VirtualFileNotFoundException;
+
+public class FileManager implements FileManagementInterface, VirtualDiskInspectionInterface{
+    @Override
+    public boolean addDirectory(String pathname, String filename) throws InvalidEntryException, VirtualFileNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean addFile(String pathname, String filename, FileType type, int length) throws InvalidEntryException, VirtualFileNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteFile(String pathname, String filename) throws InvalidEntryException, VirtualFileNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String[] listDirectory(String pathname) throws InvalidEntryException, VirtualFileNotFoundException {
+        return new String[0];
+    }
+
+    @Override
+    public boolean parseCommandFile(String pathname) {
+        return false;
+    }
+
+    @Override
+    public boolean saveVirtualDisk() {
+        return false;
+    }
+
+    @Override
+    public String getSNodeInfo(int snodeId) throws InvalidSNodeException {
+        return null;
+    }
+
+    @Override
+    public String getSnodeBitmap() {
+        return null;
+    }
+
+    @Override
+    public String getDataBlockBitmap() {
+        return null;
+    }
+}

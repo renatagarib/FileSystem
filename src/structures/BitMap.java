8dc7ea1778967e784686e24661fa3d20c9e3880b
heitorboccato <heitor.boccato@hotmail.com>
2022-07-14 17:28:42 -0300

    Modify bitMap

--- src/structures/BitMap.java
+++ src/structures/BitMap.java
@@ -8,21 +8,21 @@ public class BitMap {
     }
 
     public void addElement(int location) {
-        int place = location/8;
+        int place = location/8; //add element to the byte
         elements[place] |= (1 << (7 - location % 8));
     }
 
     public void clearElement(int location) {
-        int place = location/8;
+        int place = location/8; //clear the element
         elements[place] &= ~(1 << (7 - location % 8));
     }
 
     public byte getElement(int location) {
-        int place = location/8;
+        int place = location/8; //locate a element 
         return (byte)((elements[place] >> (7 - location % 8)) & 1);
     }
 
-    public int findClearSpot() {
+    public int findClearSpot() { //try to find a clear element
         for (int i = 0; i < (elements.length * 8); i++) {
             if (getElement(i) == 0) {
                 return i;
